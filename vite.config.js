088fea2ccad98acd83a224a5a408202b0f52ed54
Ana Oliveira <ana.oliveira@inspand.com.br>
2025-03-17 13:41:02 -0300

    update url

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/nome-do-repositorio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/sharing-on-social-media/' : '/',
 })
