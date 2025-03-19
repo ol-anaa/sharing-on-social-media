c5ffae52d3d845300635d451f79ccc4df3a18c43
Ana Oliveira <ana.oliveira@inspand.com.br>
2025-03-19 12:49:49 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -144,7 +144,7 @@ export default {
     
     async shareOnInstagram() {
 
-      window.location.href = `instagram://library?AssetPath=${this.linkCertificadoAWS}`;
+      window.location.href = `instagram://app`;
 
 
       /*
