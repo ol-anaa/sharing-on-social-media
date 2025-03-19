8d5a72689e3db35da49f2de730ca93312e434d91
Ana Oliveira <ana.oliveira@inspand.com.br>
2025-03-19 10:14:00 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -150,7 +150,7 @@ export default {
         const playStore = "https://play.google.com/store/apps/details?id=com.instagram.android";
 
         if (this.isIOS) {
-          window.location.href = `instagram://library?LocalIdentifier=${imageUri}`;
+          window.location.href = `instagram://library?LocalIdentifier=${this.linkCertificadoAWS}`;
         } else {
           window.location.href = `intent://#Intent;package=com.instagram.android;action=android.intent.action.SEND;type=image/*;S.android.intent.extra.STREAM=${imageUri};end;`;
         }
